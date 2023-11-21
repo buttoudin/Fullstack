18571a385e403c49302e634c49236b16add0df03
Clement Buttoudin <clementbut001@gmail.com>
2023-11-21 10:12:59 +0100

    .

--- POO/jarditou/employe.classe.php
+++ POO/jarditou/employe.classe.php
@@ -1,29 +1,84 @@
 <?php
-class employe
-{
-    private $Nom;
-    private $Prenom;
-    private $Date;
-    private $Fonction;
-    private $Salaire;
-    private $Service;
-    private $temps;
 
+class magasin{
 
-    public function __construct($Nom, $Prenom, $Date, $Fonction, $Salaire, $Service)
-    {
-
-        $this->Nom = $Nom;
-        $this->Prenom = $Prenom;
-        $this->Date = $Date;
-        $this->Fonction = $Fonction;
-        $this->Salaire = $Salaire;
-        $this->Service = $Service;
-        $this->calculerTempsEmploi();
-        $this->CalculPrime();
-
+    private $nom_mag;
+    private $adresse_mag;
+    private $postal_mag;
+    private $ville_mag;
+    private $restaurant;
+    
+    
+    public function __construct($nom_mag, $adresse_mag, $postal_mag, $ville_mag, $restaurant){
+        $this->nom_mag = $nom_mag;
+        $this->adresse_mag = $adresse_mag;
+        $this->postal_mag = $postal_mag;
+        $this->ville_mag = $ville_mag;
+        $this->restaurant = $restaurant;
+    }
+    public function getNom_mag(){
+        return $this->nom_mag;
+    }
+    public function getAdresse_mag(){
+        return $this->adresse_mag;
+    }
+    public function getPostal_mag(){
+        return $this->postal_mag;
+    }
+    public function getVille_mag(){
+        return $this->ville_mag;
+    }
+    public function getRestaurant(){
+        return $this->restaurant;
     }
 
+    public function VerifRestaurant(){
+        if ($this->restaurant){
+            $restau = "Il y a un restaurant d'entreprise, vous n'avez donc pas droit au ticket restaurant.";
+            return $restau;
+        }
+        else{
+            $restau = "Non il n'y a pas de restaurant d'entreprise, vous avez donc droit au ticket restaurant.";
+            return $restau;
+        }
+        
+    }
+    
+    }
+    class employe extends magasin
+    {
+        private $Nom;
+        private $Prenom;
+        private $Date;
+        private $Fonction;
+        private $Salaire;
+        private $Service;
+        private $temps;
+        private $Magasin;
+        private $enfant0_10;
+        private $enfant11_15;
+        private $enfant16_18;
+    
+        public function __construct($Nom, $Prenom, $Date, $Fonction, $Salaire, $Service, $magasin, $enfant0_10, $enfant11_15, $enfant16_18)
+        {
+            parent::__construct($magasin->getNom_mag(), $magasin->getAdresse_mag(), $magasin->getPostal_mag(), $magasin->getVille_mag(), $magasin->getRestaurant());
+    
+            $this->Nom = $Nom;
+            $this->Prenom = $Prenom;
+            $this->Date = $Date;
+            $this->Fonction = $Fonction;
+            $this->Salaire = $Salaire;
+            $this->Service = $Service;
+            $this->Magasin = $magasin;
+            $this->enfant0_10 = $enfant0_10;
+            $this->enfant11_15 = $enfant11_15;
+            $this->enfant16_18 = $enfant16_18;
+            $this->noel();
+            $this->Vacance();
+            $this->restaurant = $magasin->getRestaurant();
+            $this->calculerTempsEmploi();
+            $this->CalculPrime();
+        }
 
     public function getNom()
     {
@@ -54,6 +109,11 @@ class employe
     {
         return $this->Service;
     }
+    public function getMagasin()
+    {
+        return $this->Magasin;
+    }
+   
 
     private function calculerTempsEmploi()
     {
@@ -89,6 +149,17 @@ class employe
         }
         
     }
+    public function Vacance(){
+        if ($this->AnneeDansLentreprise > 0 ){
+            $cheque = "Vous avez le droit au chéques vacance.";
+            return $cheque;
+        }else{
+            $cheque = "Vous n'avez pas le droit au bonheur pour l'instant.";
+            return $cheque;
+        }
+
+        
+    }
     public function getVirement(){
         return $this->Virement;
     }
@@ -96,14 +167,37 @@ class employe
     {
         return $this->Prime;
     }
+    
+    public function noel(){
+        $chequeNoel = 0;
+        if ($this->enfant0_10 !=0 || $this->enfant11_15 !=0 || $this->enfant16_18 !=0){
+            for ($i=0; $i < $this->enfant0_10; $i++) { 
+                $chequeNoel = $chequeNoel + 20;
+            }
+            for ($i=0; $i < $this->enfant11_15; $i++) { 
+                $chequeNoel = $chequeNoel + 30;
+            }
+            for ($i=0; $i < $this->enfant16_18; $i++) { 
+                $chequeNoel = $chequeNoel + 50;
+            }
+            return "Vous avez le droit à " . $chequeNoel . "€, joyeux noël !";
+        }else{
+            return "Vous n'avez pas le droit au chéque de Noël.";
+        }
+    }
     public function __toString()
     {
         return "Nom : " . $this->Nom . "<br>Prenom : " . $this->Prenom . "<br>Date : " . $this->getFormatDate() .
             "<br>Fonction : " . $this->Fonction . "<br>Salaire : " . $this->Salaire . "<br>Service : " .
-            $this->Service . "<br>Année(s) dans l'entreprise : " . $this->getAnneeDansLentreprise() . "<br>Prime : " . $this->getPrime() . " " . $this->getVirement() . "<br> <br>";
+            $this->Service . "<br>Année(s) dans l'entreprise : " . $this->getAnneeDansLentreprise() . "<br>Prime : " . $this->getPrime() . " " 
+            . $this->getVirement() . "<br>Magasin :" . $this->getNom_mag() . "<br>Adresse : ". $this->getAdresse_mag() . " " .$this->getVille_mag() . " "  .
+            $this->getPostal_mag() . "<br>Restauration : ". $this->VerifRestaurant() . "<br>Chéque vacance : ". 
+            $this->vacance() . "<br>Chéques noël : ". $this->noel() . "<br> <br>";
     }
  
 
 }
+
+
 
 ?>
--- POO/jarditou/index.php
+++ POO/jarditou/index.php
@@ -1,13 +1,27 @@
 <?php
 include 'employe.classe.php';
 
-$employe1 = new employe("Tar", "Salba ", "2023-10-26 12:30:00", "Accueil", 50000, "Ressource humaine");
-$employe2 = new employe("Nom2", "Prenom2", "2021-10-25 10:45:00", "Fonction2", 60000, "Service2");
-$employe3 = new employe("Nom3", "Prenom3", "2015-10-24 08:15:00", "Fonction3", 70000, "Service3");
+$magasin1 = new magasin("champi", "3 rue de la mort", "80190", "Hell",true);
+$magasin2 = new magasin("hallucinogène", "3 rue de la vie", "80200", "heaven", true);
+$magasin3 = new magasin("jaihachebé", "15 rue de l'oublie", "45120", "clubnight",false);
+$magasin4 = new magasin("quoquahine", "3 rue de la sniff", "92100", "tarin", true);
+$magasin5 = new magasin("Olivander", "chemin de travers", "25100", "Poudlard", false);
+
+$employe1 = new employe("Deur", "Ro", "2023-10-26", "Accueil", 50000, "Ressource humaine", $magasin1,1,2,0);
+$employe2 = new employe("Ceps", "Cordie", "2021-10-25", "Fonction2", 60000, "Service2", $magasin2,0,0,0);
+$employe3 = new employe("Gus", "Fun", "2015-10-24", "Fonction3", 70000, "Service3", $magasin3,3,5,1);
+$employe4 = new employe("Queur", "Cla", "2013-10-12", "Fonction4", 26500, "Service4", $magasin4,2,0,0);
+$employe5 = new employe("Osse", "Col", "2018-03-03", "Fonction5", 23600, "Service5", $magasin5,0,0,10);
 
 echo $employe1;
 echo "<br><br>";
 echo $employe2;
 echo "<br><br>";
 echo $employe3;
+echo "<br><br>";
+echo $employe4;
+echo "<br><br>";
+echo $employe5;
 ?>
+
+
